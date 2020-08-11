8a8ff18ae73517a0caf02a4515d6bfb1ec1088e2
Asichurter <1090489718@qq.com>
2020-08-11 17:52:08 +0800

    Add class mask in plot scripts when computing logits in SIMPLE

--- visualization/um_nn_simple_comp.py
+++ visualization/um_nn_simple_comp.py
@@ -7,14 +7,24 @@ data_path = 'D:/datasets/HKS/data/multimodal-plot-data.npy'
 figsize = (10,9)
 mode = 'SIMPLE'
 
-def SIMPLE(X, Y, init_proto, init_proto_label, iter=3, sigma=0.5):
-    def cal_assigned_logits(data, protos):
+def SIMPLE(X, Y, init_proto, init_proto_label, iter=3, sigma=1):
+    def cal_assigned_logits(data, protos, p_labels):
         norm = -np.log(sigma+2*np.pi)
         data_len = len(data)
         proto_len = len(protos)
         data = np.expand_dims(data, 1).repeat(proto_len,1).reshape(data_len*proto_len,-2)
         protos = np.expand_dims(protos, 0).repeat(data_len,0).reshape(data_len*proto_len,-2)
-        logits = -((data-protos)**2).sum(-1)/(2*sigma) - norm
+
+        # generate penalization term for unmatched point and proto
+        N_INF = -100
+        class_mask_matrix = np.zeros_like(protos)
+        y_expand = np.expand_dims(Y, 1).repeat(proto_len,1).reshape(data_len*proto_len)
+        p_label_expand = np.expand_dims(p_labels, 0).repeat(data_len,0).reshape(data_len*proto_len)
+        class_mask = (y_expand!=p_label_expand)[:,None].repeat(2,-1)
+        np.putmask(class_mask_matrix, class_mask, N_INF)
+
+        # logits = (-(data-protos)**2).sum(-1)/(2*sigma) - norm
+        logits = (-((data-protos)**2)+class_mask_matrix).sum(-1)/(2*sigma) - norm
         logits = logits.reshape(data_len, proto_len)
         logits = t.softmax(t.Tensor(logits),dim=1).numpy()
         return logits
@@ -30,7 +40,7 @@ def SIMPLE(X, Y, init_proto, init_proto_label, iter=3, sigma=0.5):
                 proto_labels = np.append(proto_labels,y)
 
         # shape: [data, proto] -> [proto,data]
-        logits_ = cal_assigned_logits(X,protos).T
+        logits_ = cal_assigned_logits(X,protos,proto_labels).T
         logits_ = np.expand_dims(logits_, -1).repeat(2,-1)
         weighted_data = np.expand_dims(X,0).repeat(len(protos),0)
         protos = (logits_*weighted_data).sum(1) / logits_.sum(1)
@@ -96,3 +106,4 @@ x = x.reshape((class_num*item_num,-1))
 knn.fit(proto,proto_label)
 
 plot_decision_boundary(knn, x, y, plot_proto, proto_label)
+print('Number of proto:', len(proto))
